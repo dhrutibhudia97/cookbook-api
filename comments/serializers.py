87ba983328b8113b5b25e10f93128275f21a51e7
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-06-25 20:12:10 +0000

    python validated comment app

--- comments/serializers.py
+++ comments/serializers.py
@@ -31,7 +31,7 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
-        
+
 
 class CommentDetailSerializer(CommentSerializer):
     """
